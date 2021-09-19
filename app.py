4cc0ecdbc960aeb6813ec3fd7e17eb1aa32fc5c3
intakhab1 <amirint190@gmail.com>
2021-09-20 01:29:20 +0530

    make it better

---
+++ app.py
@@ -0,0 +1,38 @@
+
+import streamlit as st
+import pickle
+import numpy as np
+import pandas as pd
+
+# import the model
+pipe = pickle.load(open('model.pkl','rb'))
+df = pickle.load(open('df.pkl','rb'))
+
+st.title('Welcome To Car Valuation')
+
+# company
+company = st.selectbox('Company',df['company'].unique())
+
+# model
+car_model = st.selectbox('Model',df['name'].unique())
+
+# year
+year = st.selectbox('Year',df['year'].unique())
+
+# fuel_type
+fuel_type = st.selectbox('Fuel Type',df['fuel_type'].unique())
+
+# driven
+driven = st.number_input('kms driven')
+
+if st.button('Predict Price'):
+    prediction = pipe.predict(pd.DataFrame(columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'],
+                                            data=np.array([car_model, company, year, driven, fuel_type]).reshape(1, 5)))
+    print(prediction)
+    st.title( str(np.round(prediction[0], 2)))
+
+
+
+
+
+
