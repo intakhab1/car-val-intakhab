99f62d5ddf0626f192a08cee0bf62e8f5e39efe8
intakhab1 <89484385+intakhab1@users.noreply.github.com>
2021-09-21 01:10:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ pipe = pickle.load(open('model.pkl','rb'))
 df = pickle.load(open('df.pkl','rb'))
 
 st.title('Welcome To Car Valuation')
+Intakhab = f'<a href="https://github.com/intakhab1/car-val-intakhab">Developed by @Intakhab</a>'
+st.markdown(Intakhab, unsafe_allow_html=True)
 
 # company
 company = st.selectbox('Company',df['company'].unique())
